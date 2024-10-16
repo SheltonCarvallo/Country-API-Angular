1b02292e8a6730e262a47f8e99d1404463c813b4
shelton <sheltoncarvallo975@gmail.com>
2024-10-15 23:16:51 -0500

    dynamic url

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { CountryInterface } from '../../interfaces/country-interface';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  public country?: CountryInterface;
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private countryService: CountriesService
+  ) { }
+
+  /*  ngOnInit(): void {
+     //params is an obsevable
+     this.activatedRoute.params.subscribe(
+       //The parameter {id} comes from the url of the path  path: 'by/:id' in the countries-routing.module
+       ({ id }) => {
+         this.countryService.serchCountryByAlphaCode(id).subscribe(
+           country => {
+             console.log({country});
+           })
+       }); // I don't have fucking idea of this
+   } */
+  ngOnInit(): void {
+    //params is an obsevable
+    this.activatedRoute.params.subscribe( //if I am not mistaken before rendering this method fetch the id of the url and the makes a request
+      //The parameter {id} comes from the url of the path  path: 'by/:id' in the countries-routing.module
+      ({ id }) => { //id se lo obtiene por desestructurazion
+        this.searchCountryCode(id);
+      }); 
+  }
+
+  searchCountryCode(code : string){
+    this.countryService.serchCountryByAlphaCode(code)
+    .subscribe(resp => {
+      if(!resp){
+        return this.router.navigateByUrl('by-capital');
+      }
+      //console.log(resp.cca3);
+      return this.country = resp;
+    });
+  }
+
+
+  //this is another way to solve the above "problem"
+  //using rxjs
+  /*
+    this.activatedRoute.params
+    .pipe(
+      switchMap (({id}) => this.countryService.serchCountryByAlphaCode(id)
+    )
+    .subscribe(country => {
+      console.log({country})
+    });
+  */ 
 
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { CountryInterface } from "../interfaces/country-interface";
-import { catchError, Observable, of } from "rxjs";
+import { catchError, map, Observable, of } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
@@ -10,32 +10,41 @@ export class CountriesService {
 
     constructor(private httpClient: HttpClient) { } //DI
 
+    serchCountryByAlphaCode(code: string): Observable<CountryInterface | null> {
+        const url = `${this.serviceUrl}/alpha/${code}`;
+        return this.httpClient.get<CountryInterface[]>(url)
+            .pipe(
+                map(resp => resp.length > 0 ? resp[0] : null),
+                catchError(error => of(null))
+            );
+    }
+
     searchCapital(cityCharacters: string): Observable<CountryInterface[]> {
         const url = `${this.serviceUrl}/capital/${cityCharacters}`;
 
         return this.httpClient.get<CountryInterface[]>(url) //Aqui no se hace la solicitud, donde se hara la solicitud es en el codigo donde se declare subscribe
-        .pipe(
-            catchError (error => of([]))
-        );
+            .pipe(
+                catchError(error => of([]))
+            );
     }
 
 
     //https://restcountries.com/v3.1/name/{name}
-    searchCountry(countryCharacters: string): Observable<CountryInterface[]>{
-        const url= `${this.serviceUrl}/name/${countryCharacters}` //To do this url
+    searchCountry(countryCharacters: string): Observable<CountryInterface[]> {
+        const url = `${this.serviceUrl}/name/${countryCharacters}` //To do this url
         return this.httpClient.get<CountryInterface[]>(url)
-        .pipe(
-            catchError (error => of([]))
-        );
+            .pipe(
+                catchError(error => of([]))
+            );
     }
 
     //https://restcountries.com/v3.1/region/{region}
 
-    searchByRegion(regionCharacters: string): Observable<CountryInterface[]>{
+    searchByRegion(regionCharacters: string): Observable<CountryInterface[]> {
         return this.httpClient.get<CountryInterface[]>(`${this.serviceUrl}/region/${regionCharacters}`)
-        .pipe(
-            catchError(error => of([]))
-        );
+            .pipe(
+                catchError(error => of([]))
+            );
     }
 
 } 
